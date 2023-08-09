128a53d7ccd7e335d8abe5fad707d2f2eca353b5
soscooter <lelya.rumynin@gmail.com>
2023-08-09 19:10:59 +0300

    add new font

--- sRace/sRace/ViewControllers/SideViewController.swift
+++ sRace/sRace/ViewControllers/SideViewController.swift
@@ -10,23 +10,44 @@ import Lottie
 
 class SideViewController: UIViewController {
     
+    @IBOutlet weak var AboutAppTextView: UITextView!
     
     @IBOutlet weak var animatedContainerView: UIView!
+    
     var animationView: LottieAnimationView!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        playAnimation()
+        
+        setupNSAttributedString()
+    }
+    
+    
+    func playAnimation(){
         animationView = .init(name: "raceCar")
         animationView.frame = animatedContainerView.bounds
         animationView.contentMode = .scaleAspectFit
         animationView.loopMode = .loop
         animationView.animationSpeed = 0.5
         animatedContainerView.addSubview(animationView!)
-        animationView.play()    }
+        animationView.play()
+    }
     
-
+    func setupNSAttributedString() {
+        
+        guard let myFont = UIFont(name: "BetterVCR", size: 15) else {return}
+        
+        var text = AttributedString(stringLiteral: "This app is about Volkswagen Beatle")
+        
+        text.font = myFont
+        
+        AboutAppTextView.attributedText = NSAttributedString(text)
+        
+        
+        
+    }
     
-
 }
